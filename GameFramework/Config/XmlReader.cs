5e01ccf07c2d26bd240283bea486959d427a5137
Kasper Stilling Andersen <kasp762s@edu.easj.dk>
2021-04-06 11:59:30 +0200

    Xml Reader

---
+++ GameFramework/Config/XmlReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace GameFramework.Config
+{
+    public class XmlReader
+    {
+        public class GameConfig
+        {
+            //// World
+            //public int WorldSizeX { get; set; }
+            //public int WorldSizeY { get; set; }
+
+            // Player
+            public string PlayerName { get; set; }
+            public string PlayerMarker { get; set; }
+
+
+            public GameConfig(){}
+
+            public override string ToString()
+            {
+                return $"Player name: {PlayerName}, Marker: {PlayerMarker}";
+            }
+        }
+
+
+
+        public static T ReadGameConfigFile<T>()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute("GameConfig"));
+            StreamReader reader = new StreamReader("GameConfig.xml");
+            var x = (T)xmlSerializer.Deserialize(reader);
+            reader.Close();
+            
+            return x;
+        }
+
+    }
+}
--- GamePlatform/Program.cs
+++ GamePlatform/Program.cs
@@ -1,8 +1,10 @@
 ﻿using GameFramework;
+using GameFramework.Config;
 using System;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using static GameFramework.Config.XmlReader;
 
 namespace GamePlatform
 {
@@ -12,8 +14,14 @@ namespace GamePlatform
         {
             // Console.SetCursorPosition(0,0); No flickering in console window, also you can control placement of objects this way.
 
-            Game game = new Game();
-            game.Start();
+            //Game game = new Game();
+            //game.Start();
+
+
+
+            GameConfig conf = ReadGameConfigFile<GameConfig>();
+            Console.WriteLine(conf.PlayerName+  ", "+ conf.PlayerMarker);
+            Console.ReadLine();
         }
 
     }
